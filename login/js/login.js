4f376d38a0d1a82ae5e3fa5ac55ebbde5fc94621
David <jose.aceituno.balconi@gmail.com>
2024-08-07 15:51:48 -0600

    login

---
+++ login/js/login.js
@@ -0,0 +1,26 @@
+
+let DOM = document.querySelector('#root')
+DOM.innerHTML=`
+<div class="login">
+<button class="btnGIT"> Github</button>
+<div class="ventana">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2j_SKgOLm9nz6-2REBbq8f6-ycRGHYL21Ew&s" alt="" class="img">
+    <div class="log">
+        <div class="hola">Hola</div>
+        <div class="bienvenido"> Bienvenido al MultiTasking</div>
+        <div class="titulo">iniciar sesion</div>
+
+        <input type="text" class="entrada_de_texto_1" placeholder="Contraseña">
+        <input type="text" class="entrada_de_texto_2" placeholder="Correo">
+
+        <a href="http://127.0.0.1:5500/main.html" class="beta">
+            <button>Probar Beta</button>
+        </a>
+        <button class="iniciar">Iniciar</button>
+
+        <div class="crear">Crear Cuenta</div>
+    </div>
+</div>
+</div>
+
+`
